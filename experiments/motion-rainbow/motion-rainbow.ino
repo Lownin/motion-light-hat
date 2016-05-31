438f66eea7278fde83f5707f634d888c0ec40233
Adrian Acosta <adrian@Adrians-MBP.ngf>
2016-05-30 19:57:27 -0700

    Added position tracking code to motion-rainbow. Seems to work.

--- experiments/motion-rainbow/motion-rainbow.ino
+++ experiments/motion-rainbow/motion-rainbow.ino
@@ -1,84 +1,71 @@
+#include <Wire.h>
+#include <L3G.h>
 #include <Adafruit_NeoPixel.h>
-#define PIN 6
-#include <Wire.h> 
-#include <Adafruit_L3GD20.h>
-Adafruit_L3GD20 gyro;
 
-float pos=0;
-unsigned long last_time, this_time, diff_time;
+
+#define PIN 6
+
+L3G gyro;
 
 // Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
+// Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+int pos=0;
+int pos256=0;
+unsigned long last_time, this_time, diff_time;
 
 void setup() {
-  Serial.begin(9600);
   while (!Serial && (millis ()  <= 3000)) ;
-  if (!gyro.begin(gyro.L3DS20_RANGE_250DPS))
-    {
-    Serial.println("Oops ... unable to initialize the L3GD20. Check your wiring!");
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!gyro.init())
+  {
+    Serial.println("Failed to autodetect gyro type!");
     while (1);
   }
+  last_time=millis();
+  gyro.enableDefault();
+  strip.setBrightness(30);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(10);
-  last_time=millis();
 }
 
 void loop() {
-//  colorClear(strip.Color(0, 0, 0), 50); // Off
-//  colorWipe(strip.Color(255, 0, 0), 1); // Red
-//  colorWipe(strip.Color(0, 255, 0), 1); // Blue
-//  colorWipe(strip.Color(0, 0, 255), 1); // Green
-  rainbowCycle(500);
+  rainbowCycle(20);
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
-  uint16_t i, j, g;
+  uint16_t i;
+  gyro.read();
+  this_time=millis();
+  delay(10);
+  diff_time=this_time-last_time;
+  last_time=this_time;
+  if (gyro.g.z>300||gyro.g.z<-200)
+    {pos+=diff_time*gyro.g.z;}
+  pos256=pos/156250;
+  Serial.print("Z: "); Serial.print(gyro.g.z);   Serial.print(" ");
+  Serial.print("POS: "); Serial.print(pos);   Serial.print(" ");
+  Serial.print("POS256: "); Serial.println(pos256);   Serial.print(" ");
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      gyro.read();
-      this_time=millis();
-      diff_time=this_time-last_time;
-      last_time=this_time;
-      if (gyro.data.z<1100)
-        {pos+=(diff_time+20)*gyro.data.z/92160;}
-      if (gyro.data.z>1100)
-        {pos-=diff_time*gyro.data.z/92160;}
-      g=((int)pos);
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + g) & 255));
-//      Serial.print("Z: "); Serial.println((int)gyro.data.z); Serial.print(" ");
-      Serial.print("POS: "); Serial.println((int)g); Serial.print(" ");
-    }
-    strip.show();
-//    delay(10);
+  for(i=0; i< strip.numPixels(); i++) {
+    strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + pos256) & 255));
   }
-}
 
-// Eliminate all color for fun!
-void colorClear(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
+  strip.show();
 }
 
 // Input a value 0 to 255 to get a color value.
