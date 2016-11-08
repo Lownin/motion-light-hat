4422f4a10f7be1dd26caa1bfeca23051666a444b
Adrian Acosta <adrian@Adrians-MBP.ngf>
2016-11-08 14:38:00 -0800

    converted strip test to red white blue.

--- experiments/election-day/election-day.ino
+++ experiments/election-day/election-day.ino
@@ -1,10 +1,9 @@
-#include <Wire.h>
-#include <L3G.h>
 #include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
 #define PIN 7
-
-L3G gyro;
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -14,69 +13,102 @@ L3G gyro;
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(39, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
-int led = 13;
-int pos=0;
-int pos256=0;
-unsigned long last_time, this_time, diff_time;
-
 void setup() {
-  pinMode(9, INPUT_PULLUP);
-  // initialize the digital pin connected to teensy LED as an output.
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  while (!Serial && (millis ()  <= 3000)) ;
-  Serial.begin(9600);
-  Wire.begin();
-
-  if (!gyro.init())
-  {
-    Serial.println("Failed to autodetect gyro type!");
-    while (1);
-  }
-  last_time=millis();
-  gyro.enableDefault();
-  strip.setBrightness(30);
   strip.begin();
+  strip.setBrightness(16);
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  if (digitalRead(9) == HIGH) {
-    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-    rainbowCycle();
-  } 
-  else {
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-    Serial.println("Button pressed!!!");
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(255, 255, 255), 50); // White
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127, 0, 0), 50); // Red
+  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+
+  rainbow(20);
+  rainbowCycle(20);
+  theaterChaseRainbow(50);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
 
-void rainbowCycle() {
-  uint16_t i;
-  gyro.read();
-  this_time=millis();
-  delay(10);
-  diff_time=this_time-last_time;
-  last_time=this_time;
-  if (gyro.g.y>0 ||gyro.g.y<-700)
-    {pos-=diff_time*gyro.g.y*5;}
-  pos256=pos/156250;
-  Serial.print("Y: "); Serial.print(gyro.g.y);   Serial.print(" ");
-  Serial.print("POS: "); Serial.print(pos);   Serial.print(" ");
-  Serial.print("POS256: "); Serial.println(pos256);   Serial.print(" ");
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
 
-  for(i=0; i< strip.numPixels(); i++) {
-    strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + pos256) & 255));
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      //strip.setPixelColor(i, Wheel((i+j) & 255));
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
   }
+}
 
-  strip.show();
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(20);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -84,12 +116,12 @@ void rainbowCycle() {
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return strip.Color(255, 0, 0);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(255, 255, 255);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return strip.Color(0, 0, 255);
 }
